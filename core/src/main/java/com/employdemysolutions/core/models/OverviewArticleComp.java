f1c233664043800c91550ef62528e2346303d036
siddharthopo <147807271+siddharthopo@users.noreply.github.com>
2024-02-01 15:57:23 +0530

    new file

--- core/src/main/java/com/employdemysolutions/core/models/OverviewArticleComp.java
+++ core/src/main/java/com/employdemysolutions/core/models/OverviewArticleComp.java
@@ -2,26 +2,22 @@ package com.employdemysolutions.core.models;
 
 import com.employdemysolutions.core.service.PageInfo;
 import com.employdemysolutions.core.service.PageQueryService;
-import org.apache.sling.api.SlingHttpServletRequest;
+
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 
 import java.util.List;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.employdemysolutions.core.service.PageInfo;
-import com.employdemysolutions.core.service.PageQueryService;
-
-@Model
-   ( adaptables =Resource.class)
-
+// @Model
+//    ( adaptables =Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class  OverviewArticleComp   {
     private static final Logger LOG = LoggerFactory.getLogger(OverviewArticleComp .class);
     @ValueMapValue
